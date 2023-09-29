325028b1f7d588cc3d0714177030059ec15d535b
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-09-29 08:53:32 +0300

    feat: init commit

---
+++ src/main/java/ru/vlasov/englishstudy/EnglishStudyApplication.java
@@ -0,0 +1,13 @@
+package ru.vlasov.englishstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vlasov/englishstudy/EnglishStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlasov.englishstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
